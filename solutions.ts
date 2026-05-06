17958f9d99a5c1e89ebec4d0e172b12416b10c70
Md Abdur Rahman Rakib <rakiblp6@gmail.com>
2026-05-07 02:04:11 +0600

    seventh

--- solutions.ts
+++ solutions.ts
@@ -62,3 +62,30 @@ console.log(myBook);
 
 // Problem - 6
 
+class Person {
+        name: string;
+        age: number;
+
+        constructor(name: string, age: number){
+                this.name = name,
+                this.age = age
+        }
+}
+
+class Student extends Person {
+        grade: string;
+        
+        constructor(name: string, age: number, grade: string){
+                super(name, age)
+                this.grade = grade
+
+        }
+        getDetails(){
+                console.log(`Name: ${this.name}, Age: ${this.age}, Grade: ${this.grade}`)
+        }
+}
+
+const student = new Student("Alice", 20, "A");
+student.getDetails();
+
+// Problem - 7
